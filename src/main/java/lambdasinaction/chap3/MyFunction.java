f6a98873b8920edc029ce6ebb07e345a1cb0a953
xinyuexy <1151980485@qq.com>
2020-03-07 12:09:05 +0800

    lambda exercise

--- src/main/java/lambdasinaction/chap3/MyFunction.java
+++ src/main/java/lambdasinaction/chap3/MyFunction.java
@@ -1,4 +1,51 @@
 package lambdasinaction.chap3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * 使用JDK中自带的函数式接口
+ */
 public class MyFunction {
+    public static void main(String[] args) {
+        //1. Predicate: 需要一个涉及类型T的布尔表达式时可以使用
+        List<String> list = Arrays.asList("asc", "", "lxy");
+        List<String> res = filter(list, (String s) -> !s.isEmpty());
+        System.out.println(res);
+
+        //2. Consumer: 提供一个没有参数和返回值的accept方法,适用于对列表中的元素执行一些操作
+        forEach(list, (String i) -> System.out.println(i));
+
+        //3. Function: 可以实现一个map,如把字符串列表转换为其长度列表
+        List<Integer> lens = map(list, (String s) -> s.length());
+        System.out.println(lens);
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> results = new ArrayList<>();
+        for (T s: list) {
+            if (p.test(s)) {
+                results.add(s);
+            }
+        }
+        return results;
+    }
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T i: list) {
+            c.accept(i);
+        }
+    }
+
+    public static <T, R> List<R> map(List<T> list, Function<T, R> f) {
+        List<R> result = new ArrayList<>();
+        for (T s: list) {
+            result.add(f.apply(s));
+        }
+        return result;
+    }
 }
--- src/main/java/lambdasinaction/chap3/MyProcessFile.java
+++ src/main/java/lambdasinaction/chap3/MyProcessFile.java
@@ -1,4 +1,45 @@
 package lambdasinaction.chap3;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * 使用lambda表达式处理文件
+ */
 public class MyProcessFile {
+    public static void main(String[] args) throws IOException {
+        String fileName = "lambdasinaction/chap3/data.txt";
+        String result = processFileLimited(fileName);
+        System.out.println(result);
+
+        System.out.println("---");
+
+        //读取一行
+        String oneLine = processFile((BufferedReader br) -> br.readLine(), fileName);
+        System.out.println(oneLine);
+
+        //读取两行
+        BufferedReaderProcessor p = (BufferedReader br) -> br.readLine() + br.readLine();
+        String twoLines = processFile(p, fileName);
+        System.out.println(twoLines);
+    }
+
+    public static String processFileLimited(String fileName) throws IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            return br.readLine();
+        }
+    }
+
+    /** 接受lambda表达式,灵活处理 */
+    public static String processFile(BufferedReaderProcessor p, String fileName) throws IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            return p.process(br);   //其他操作照常,把处理文件的行为参数化(接受参数和返回)
+        }
+    }
+
+    public interface BufferedReaderProcessor {
+        public String process(BufferedReader b) throws IOException;
+    }
 }
